4798b658c4e2a407747ec37c1ffeeed9d7fc73ba
JackDuncan15 <Duncan-J15@ulster.ac.uk>
2025-12-01 17:28:37 +0000

    entire current app

--- app/src/main/java/com/example/courseworkjplapp/LoginScreen.java
+++ app/src/main/java/com/example/courseworkjplapp/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.example.courseworkjplapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,19 +9,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
 public class LoginScreen extends AppCompatActivity {
-    DatabaseHelper db;
     EditText edittxt_Email,edittxt_Password;
     Button btn_Login,btn_Register;
+    FirebaseAuth mAuth;
+    DatabaseReference usersRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        db = new DatabaseHelper(this);
 
         edittxt_Email = (EditText) findViewById(R.id.edittxt_Email);
         edittxt_Password = (EditText) findViewById(R.id.edittxt_Password);
+        mAuth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance("https://fir-authentication25-85f05-default-rtdb.europe-west1.firebasedatabase.app/").getReference("users");
 
         btn_Register = (Button) findViewById(R.id.btn_Register);
         btn_Register.setOnClickListener(new View.OnClickListener() {
@@ -31,31 +42,37 @@ public class LoginScreen extends AppCompatActivity {
             }
         });
         btn_Login = (Button) findViewById(R.id.btn_Login);
-        btn_Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String Email = edittxt_Email.getText().toString();
-                String Password = edittxt_Password.getText().toString();
+        btn_Login.setOnClickListener(view ->{
+            String Email = edittxt_Email.getText().toString();
+            String Password = edittxt_Password.getText().toString();
 
-                if (Email.equals("")||Password.equals("")){
-                    Toast.makeText(LoginScreen.this, "Fields are empty", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Boolean checkEmail = db.checkEmail(Email);
-                    if (checkEmail == true){
-                        Toast.makeText(LoginScreen.this, "Email is correct", Toast.LENGTH_SHORT).show();
-                        Boolean checkPassword = db.checkPassword(Email,Password);
-                        if (checkPassword == true){
-                            Toast.makeText(LoginScreen.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(LoginScreen.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
-                        }
+            if (Email.equals("")||Password.equals("")){
+                Toast.makeText(this, "Fields are empty", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                mAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(task ->{
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        String uid = user.getUid();
+                        usersRef.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                String email = snapshot.child("email").getValue(String.class);
+                                Intent i = new Intent(LoginScreen.this,MainActivity.class);
+                                startActivity(i);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(LoginScreen.this, "Error Login Fail", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
                     else {
-                        Toast.makeText(LoginScreen.this, "No account", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show();
                     }
-                }
+                });
             }
         });
     }
--- app/src/main/java/com/example/courseworkjplapp/Register.java
+++ app/src/main/java/com/example/courseworkjplapp/Register.java
@@ -9,20 +9,29 @@ import android.view.View;
 import android.widget.Toast;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.util.HashMap;
+
 public class Register extends AppCompatActivity {
     EditText ET_Email,ET_Password,ET_ConfirmPassword;
     Button btn_RegisterAcc;
-    DatabaseHelper db;
+    FirebaseAuth mAuth;
+    DatabaseReference usersRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        db = new DatabaseHelper(this);
         ET_Email = (EditText)findViewById(R.id.edittxt_RegEmail);
         ET_Password = (EditText) findViewById(R.id.edittxt_RegPassword);
         ET_ConfirmPassword = (EditText) findViewById(R.id.edittxt_ConfirmPassword);
-
+        usersRef = FirebaseDatabase.getInstance("https://fir-authentication25-85f05-default-rtdb.europe-west1.firebasedatabase.app/").getReference("users");
+        mAuth = FirebaseAuth.getInstance();
         btn_RegisterAcc = (Button) findViewById(R.id.btn_RegisterAcc);
         btn_RegisterAcc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,22 +44,26 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this, "Fields are empty", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    if (Password.equals(Confirm)){
-                        Boolean checkEmail = db.checkEmail(Email);
-                        if (checkEmail==false){
-                            Boolean insert = db.insert(Email,Password);
-                            if (insert == true) {
-                                Toast.makeText(Register.this, "Registration Successful", Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(Register.this,LoginScreen.class);
-                                startActivity(i);
+                    if (Password.equals(Confirm)) {
+                        mAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(task->{
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if (user != null){
+                                    String uid = user.getUid();
+                                    HashMap<String,Object> userData = new HashMap<>();
+                                    userData.put("email",Email);
+                                    userData.put("uid",uid);
+                                    usersRef.child(uid).setValue(userData);
+                                    Toast.makeText(Register.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(Register.this,LoginScreen.class);
+                                    startActivity(i);
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(Register.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                            else{
-                                Toast.makeText(Register.this, "Not Registered", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else{
-                            Toast.makeText(Register.this, "User already exists", Toast.LENGTH_SHORT).show();
-                        }
+                        });
                     }
                     else{
                         Toast.makeText(Register.this, "Passwords don't match", Toast.LENGTH_SHORT).show();
